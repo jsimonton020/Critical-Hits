40948e8430447a9c704366c42e7144e4380c7873
jsimonton020 <jsimonton020@gmail.com>
2016-09-12 15:02:24 -0700

    added crit statements, and a 'draw' conclusion

--- critical_hits.py
+++ critical_hits.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 import random
 import sys
+import os
 
 CRIT = 6
+#gen_hp = random.randint(30, 100)
+#max_gen = gen_hp
 
 class Character:
     def __init__(self, state, name, hp, hp_max):
@@ -32,23 +35,44 @@ while True:
     choice = input("Make a selection: ")
 
     if choice == "1":
+        os.system('clear')
         p_roll = Character.dice_roll()
         print(p_roll)
         print("You hit for", sum(p_roll))
+        if sum(p_roll) >= 6 and sum(p_roll) < 12:
+            print("CRITICAL HIT!")
+        if sum(p_roll) >= 12 and sum(p_roll) < 18:
+            print("MEGA HIT!")
+        if sum(p_roll) >= 18:
+            print("M-M-M-M-M-MONSTER HIT!")
         enemy.hp = enemy.hp - sum(p_roll)
         e_roll = Character.dice_roll()
         print(e_roll)
         print("Enemy hit for", sum(e_roll))
+        if sum(e_roll) >= 6 and sum(e_roll) < 12:
+            print("CRITICAL HIT!")
+        if sum(e_roll) >= 12 and sum(e_roll) < 18:
+            print("MEGA HIT!")
+        if sum(e_roll) >= 18:
+            print("M-M-M-M-M-MONSTER HIT!")
         player.hp = player.hp - sum(e_roll)
-        if enemy.hp <= 0:
-                print("\nEnemy Down!\n")
-                break
-        if player.hp <= 0:
-                print("\nYou died!\n")
-                break
+        if enemy.hp <= 0 and player.hp > 0:
+            print("\nEnemy Down!\n")
+            print("Player hit points:", player.hp)
+            break
+        if player.hp <= 0 and enemy.hp > 0:
+            print("\nYou died!\n")
+            print("Enemy hit points:", enemy.hp)
+            break
+        if player.hp and enemy.hp <= 0:
+            print("\nDraw!\n")
+            print("Player hit points:", player.hp)
+            print("Enemy hit points:", enemy.hp)
+            break
 
     elif choice == "2":
         print("Closing Critical Hits")
         sys.exit()
     else:
         print("\nPlease choose from the list\n")
+        os.system('clear')

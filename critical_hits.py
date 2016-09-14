e428cd44d9270954c999456cd2b6f47e6defa57f
jsimonton020 <jsimonton020@gmail.com>
2016-09-14 09:49:57 -0700

    cleaned up version

--- critical_hits.py
+++ critical_hits.py
@@ -4,16 +4,13 @@ import sys
 import os
 
 CRIT = 6
-#gen_hp = random.randint(30, 100)
-#max_gen = gen_hp
+gen_hp = random.randint(50, 100)
 
 class Character:
-    def __init__(self, state, name, hp, hp_max):
-        self.state = state
+    def __init__(self,name, hp):
         self.name = name
         self.hp = hp
-        self.hp_max = hp_max
-    def dice_roll():
+    def dice_roll(self):
         dice_numbers = []
         dice = random.randint(1,6)
         dice_numbers.append(dice)
@@ -22,23 +19,25 @@ class Character:
             dice_numbers.append(dice)
         return dice_numbers
 
-player = Character("normal", "player", 50, 50)
-enemy = Character("normal", "enemy", 50, 50)
+player = Character("Player", gen_hp)
+enemy = Character("Enemy", gen_hp)
 
+os.system('clear')
 print("\nWelcome to Critical Hits!\n")
 
 while True:
-    print("\nTarget hit points:", enemy.hp)
-    print("Player hit points:", player.hp)
+    print("\n")
+    print(enemy.name, "hit points:", enemy.hp)
+    print(player.name, "hit points:", player.hp)
     print("1: Roll")
     print("2: Quit")
     choice = input("Make a selection: ")
 
     if choice == "1":
         os.system('clear')
-        p_roll = Character.dice_roll()
+        p_roll = player.dice_roll()
         print(p_roll)
-        print("You hit for", sum(p_roll))
+        print(player.name, "hit for", sum(p_roll))
         if sum(p_roll) >= 6 and sum(p_roll) < 12:
             print("CRITICAL HIT!")
         if sum(p_roll) >= 12 and sum(p_roll) < 18:
@@ -46,9 +45,10 @@ while True:
         if sum(p_roll) >= 18:
             print("M-M-M-M-M-MONSTER HIT!")
         enemy.hp = enemy.hp - sum(p_roll)
-        e_roll = Character.dice_roll()
+        e_roll = enemy.dice_roll()
+        print("\n")
         print(e_roll)
-        print("Enemy hit for", sum(e_roll))
+        print(enemy.name, "hit for", sum(e_roll))
         if sum(e_roll) >= 6 and sum(e_roll) < 12:
             print("CRITICAL HIT!")
         if sum(e_roll) >= 12 and sum(e_roll) < 18:
@@ -57,22 +57,34 @@ while True:
             print("M-M-M-M-M-MONSTER HIT!")
         player.hp = player.hp - sum(e_roll)
         if enemy.hp <= 0 and player.hp > 0:
-            print("\nEnemy Down!\n")
-            print("Player hit points:", player.hp)
+            print(enemy.name, "Down!\n")
+            print(player.name, "hit points:", player.hp)
             break
         if player.hp <= 0 and enemy.hp > 0:
             print("\nYou died!\n")
-            print("Enemy hit points:", enemy.hp)
+            print(enemy.name, "hit points:", enemy.hp)
             break
         if player.hp and enemy.hp <= 0:
             print("\nDraw!\n")
-            print("Player hit points:", player.hp)
-            print("Enemy hit points:", enemy.hp)
+            print(player.name, "hit points: 0")
+            print(enemy.name, "hit points: 0")
             break
 
     elif choice == "2":
         print("Closing Critical Hits")
         sys.exit()
     else:
-        print("\nPlease choose from the list\n")
         os.system('clear')
+        print("\nPlease choose from the list\n")
+
+def restart_p():
+    restart = input("Would you like to play again? >")
+    if restart == "yes" or restart == "y":
+        os.execl(sys.executable, sys.executable, *sys.argv)
+    elif restart == "no" or restart == "n":
+        print("Closing Critical Hits")
+        sys.exit()
+    else:
+        print("invalid input")
+        restart_p()
+restart_p()
